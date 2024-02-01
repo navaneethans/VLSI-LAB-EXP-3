a12d742a9b3f74edc828d03f6bca812132ffc8f8
navaneethans <navi.gct10@gmail.com>
2024-02-01 14:52:30 +0530

    updatefiles

---
+++ Exp3_Multiplier_2bit.v
@@ -0,0 +1,12 @@
+module ha(a,b,sum,carry);
+input a,b;
+output sum,carry;
+
+endmodule
+
+module multi_2(a,b,p,carry);
+input [1:0]a,b;
+output [2:0]p;
+output carry;
+
+endmodule
---
+++ Exp3_Multiplier_4bit.v
@@ -0,0 +1,24 @@
+module ha(a,b,sum,carry);
+input a,b;
+output sum,carry;
+
+
+endmodule
+
+module fa_ha(a,b,c,sum,carry);
+input a,b,c;
+output sum,carry;
+
+
+endmodule
+
+module multi_4(a,b,p,carry);
+input[3:0]a,b;
+output [6:0]p;
+output carry;
+wire [17:1]w;
+
+
+
+
+endmodule
